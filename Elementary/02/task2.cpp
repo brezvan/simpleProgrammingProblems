1520d38218cb39ff11455b647031903ecee95eaa
Peter <decker@lapter.(none)>
2013-12-09 18:27:37 +0100

    task2

---
+++ Elementary/02/task2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+String name;
+cout<<"please enter your name"<<endl;
+getline(cin,name);
+cout<<"welcome to the jungle:"<<name<<endl;
+return 0;
+}
